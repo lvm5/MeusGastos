ee529db9e5082b50ba21a42251e1671b8551d13a
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2024-02-06 13:35:07 -0300

    Modified local currency

---
+++ MeusGastos/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  MeusGastos
+//
+//  Created by Leandro Morais on 2024.02.06.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
