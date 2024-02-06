2c0722153ee7b6bed18903a47555b484f88b9e16
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2024-02-06 12:57:23 -0300

    Initial Commit

---
+++ MeusGastos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MeusGastos
+//
+//  Created by Leandro Morais on 2024.02.06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MeusGastos/MeusGastosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeusGastosApp.swift
+//  MeusGastos
+//
+//  Created by Leandro Morais on 2024.02.06.
+//
+
+import SwiftUI
+
+@main
+struct MeusGastosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
